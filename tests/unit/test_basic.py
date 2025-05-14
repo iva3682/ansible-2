55dc2bb2ae7eac1e15ab66bb6b787791bab3cebb
iva3682 <iva3682@yandex.ru>
2025-05-14 09:20:29 +0500

    fisrt commit

---
+++ tests/unit/test_basic.py
@@ -0,0 +1,10 @@
+"""Unit tests for devops_81.hw_03."""
+
+
+def test_basic() -> None:
+    """Dummy unit test that always passes.
+
+    Raises:
+        AssertionError: If the assertion fails.
+    """
+    assert bool(1) is True
